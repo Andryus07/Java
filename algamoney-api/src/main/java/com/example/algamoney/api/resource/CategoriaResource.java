e7c89c190bd7c61dfcfce8d3c2fbb880d4519e3d
Andryus Oliveira <andryus.oliveira@hotmail.com>
2018-01-26 18:42:15 -0200

    Projeto Api Commit

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,55 @@
+package com.example.algamoney.api.resource;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoney.api.model.Categoria;
+import com.example.algamoney.api.repository.CategoriasRepository;
+import com.mysql.fabric.Response;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	// injetando a classe categoriaRepostorio para trazer seus recursos
+	@Autowired
+	private CategoriasRepository categoriaRepository;
+
+	// mapeamento do get para essa categoria
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+
+	// mapeamento para criar um recurso no banco de dados
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	//Post com regra para usuário que informar valores null, com retorno de erro 404.
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build(); 
+	}
+}
