ef0471783e6b6086f8d6c195e4bf7b008e1ffeb0
Andryus Oliveira <andryus.oliveira@hotmail.com>
2018-02-08 23:52:51 -0200

    Continuando projeto, criação de validações do formulário

--- brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,9 +1,27 @@
 package com.algaworks.brewer.model;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 public class Cerveja {
 
+	@NotBlank(message = "SKU vazio")
 	private String sku;
+
+	@NotBlank(message = "Nome vazio")
 	private String nome;
+
+	@Size(max = 50, message = "Limite de caracter")
+	private String descricao;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
 	public String getSku() {
 		return sku;
