14796a817a16f84facc217c2e2975c8754121ae6
Andryus Oliveira <andryus.oliveira@hotmail.com>
2018-02-10 10:54:03 -0200

    Iniciando com o layout responsivo(cadastro-produto)

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -30,4 +30,10 @@ public class CervejasController {
 		System.out.println("Descricao: " + cerveja.getDescricao());
 		return "redirect:/cervejas/novo";
 	}
+
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
+	}
+
 }
