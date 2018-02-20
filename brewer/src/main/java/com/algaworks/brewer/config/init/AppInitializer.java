ab39f60bf39870708b61711f14fa83669a2c6919
Andryus Oliveira <andryus.oliveira@hotmail.com>
2018-02-19 23:19:36 -0300

    Mexendo no Layout

--- brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -1,12 +1,8 @@
 package com.algaworks.brewer.config.init;
 
-import java.nio.charset.CharacterCodingException;
-
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.algaworks.brewer.config.WebConfig;
-import com.fasterxml.classmate.Filter;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
